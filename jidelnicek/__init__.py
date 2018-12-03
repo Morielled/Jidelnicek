f25a7bbcce3ecf0733f101f617866d06a61f0024
Morielled <45030248+Morielled@users.noreply.github.com>
2018-12-03 15:38:42 +0100

    update zobrazovani ingredienci

--- jidelnicek/__init__.py
+++ jidelnicek/__init__.py
@@ -53,12 +53,18 @@ def jidelnicek_plan():
 		
 		#zahrnuti ingredienci do receptu
 		ingre1 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
-		ingre2 = Ingredience.query.filter_by(recepty_id = nazev2.id).all()
-		ingre3 = Ingredience.query.filter_by(recepty_id = nazev3.id).all()
-		ingre4 = Ingredience.query.filter_by(recepty_id = nazev4.id).all()
-		ingre5 = Ingredience.query.filter_by(recepty_id = nazev5.id).all()
+		ingre2 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
+		ingre3 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
+		ingre4 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
+		ingre5 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
 
-		return render_template('jidelnicek2.html', nazev1=nazev1, nazev2=nazev2, nazev3=nazev3, nazev4=nazev4, nazev5=nazev5, ingre1=ingre1, ingre2=ingre2, ingre3=ingre3, ingre4=ingre4, ingre5=ingre5)
+		ingre11 = [item[0] for item in ingre1]
+		ingre21 = [item[0] for item in ingre2]
+		ingre31 = [item[0] for item in ingre3]
+		ingre41 = [item[0] for item in ingre4]
+		ingre51 = [item[0] for item in ingre5]
+		
+		return render_template('jidelnicek2.html', nazev1=nazev1, nazev2=nazev2, nazev3=nazev3, nazev4=nazev4, nazev5=nazev5, ingre11=ingre11, ingre21=ingre21, ingre31=ingre31, ingre41=ingre41, ingre51=ingre51)
 
 ''' dalsi pokus o random
 @app.route('/jidelnicek_plan')
