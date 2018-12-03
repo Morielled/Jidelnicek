cabfccf3d9bfdc351cd21baa8ec454fdfb694c6c
Morielled <45030248+Morielled@users.noreply.github.com>
2018-12-03 16:05:49 +0100

    jidelnicek2 update

--- jidelnicek/__init__.py
+++ jidelnicek/__init__.py
@@ -53,10 +53,10 @@ def jidelnicek_plan():
 		
 		#zahrnuti ingredienci do receptu
 		ingre1 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
-		ingre2 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
-		ingre3 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
-		ingre4 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
-		ingre5 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
+		ingre2 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev2.id).all()
+		ingre3 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev3.id).all()
+		ingre4 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev4.id).all()
+		ingre5 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev5.id).all()
 
 		ingre11 = [item[0] for item in ingre1]
 		ingre21 = [item[0] for item in ingre2]
@@ -66,45 +66,37 @@ def jidelnicek_plan():
 		
 		return render_template('jidelnicek2.html', nazev1=nazev1, nazev2=nazev2, nazev3=nazev3, nazev4=nazev4, nazev5=nazev5, ingre11=ingre11, ingre21=ingre21, ingre31=ingre31, ingre41=ingre41, ingre51=ingre51)
 
-''' dalsi pokus o random
-@app.route('/jidelnicek_plan')
-def jidelnicek_plan():
-		
-		nazev = Recepty.query.options(load_only('id')).offset(
-            func.floor(
-                func.random() *
-                db.session.query(func.count(Recepty.id))
-            )
-        		).limit(1).all() 
-    return render_template('jidelnicek2.html', nazev=nazev)
-'''
-
-'''
-@app.route('/jidelnicek_plan')
-def jidelnicek_plan():
-		nazev = Recepty.query.order_by(func.random()).first()
-#zahrnuti ingredienci do receptu
-		#ingre = Ingredience.query.filter_by(recepty_id = nazev.id).all()
-		ingre = db.session.query(Recepty, Ingredience)\
-														.join(Ingredience, Ingredience.recepty_id == Recepty.id).all()
-		if nazev.id == ingre:
-					return render_template('jidelnicek2.html', ingre=ingre)
-		else:
-					return render_template('jidelnicek2.html', nazev=nazev)
-
-'''
-#my_ingredience.recepty
 
 #nove generovani receptu
 @app.route('/jidelnicek_plan_new')
 def jidelnicek_plan_new():
+		# 5 ruznych random receptu
 		nazev1 = Recepty.query.order_by(func.random()).first()
 		nazev2 = Recepty.query.order_by(func.random()).first()
 		nazev3 = Recepty.query.order_by(func.random()).first()
 		nazev4 = Recepty.query.order_by(func.random()).first()
 		nazev5 = Recepty.query.order_by(func.random()).first()
 		
-		return render_template('jidelnicek2.html', nazev1=nazev1, nazev2=nazev2, nazev3=nazev3, nazev4=nazev4, nazev5=nazev5)
+		#zahrnuti ingredienci do receptu
+		ingre1 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
+		ingre2 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev2.id).all()
+		ingre3 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev3.id).all()
+		ingre4 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev4.id).all()
+		ingre5 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev5.id).all()
+
+		ingre11 = [item[0] for item in ingre1]
+		ingre21 = [item[0] for item in ingre2]
+		ingre31 = [item[0] for item in ingre3]
+		ingre41 = [item[0] for item in ingre4]
+		ingre51 = [item[0] for item in ingre5]
+		
+		return render_template('jidelnicek2.html', nazev1=nazev1, nazev2=nazev2, nazev3=nazev3, nazev4=nazev4, nazev5=nazev5, ingre11=ingre11, ingre21=ingre21, ingre31=ingre31, ingre41=ingre41, ingre51=ingre51)
+
+#vygenerovani nakupniho seznamu
+@app.route('/jidelnicek_plan_seznam')
+def jidelnicek_plan_seznam():
+
+		return render_template('jidelnicek3.html')
 
 
 @app.errorhandler(404)
