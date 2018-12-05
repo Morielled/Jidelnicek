91cb57a0b9162528407017cab0b7a2e158b9b14c
Morielled <45030248+Morielled@users.noreply.github.com>
2018-12-05 08:36:28 +0100

    pokus

--- jidelnicek/__init__.py
+++ jidelnicek/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, session, request, redirect
 from flask import render_template,url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func, select
+from sqlalchemy.orm import sessionmaker
 
 from jidelnicek.forms import IngreForm1, IngreForm2
 #import pdfkit
@@ -54,6 +55,16 @@ def jidelnicek_plan():
 		nazev4 = Recepty.query.order_by(func.random()).first()
 		nazev5 = Recepty.query.order_by(func.random()).first()
 
+		#neopakovat recepty, dodelat
+		if nazev1 == nazev2:
+			nazev2 == Recepty.query.order_by(func.random()).first()
+			if nazev2 == nazev3:
+					nazev3 == Recepty.query.order_by(func.random()).first()
+					if nazev3 == nazev4:
+							nazev4 == Recepty.query.order_by(func.random()).first()
+							if nazev4 == nazev5:
+									nazev5 == Recepty.query.order_by(func.random()).first()
+
 		#zahrnuti ingredienci do receptu
 		ingre1 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
 		ingre2 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev2.id).all()
@@ -68,9 +79,8 @@ def jidelnicek_plan():
 		ingre51 = [item[0] for item in ingre5]
 
 		form1 = IngreForm1()
-
 		
-
+	
 		if 'nazev1' in session:
 			nazev1 = session ['nazev1']
 		elif 'nazev2' in session:
@@ -81,17 +91,16 @@ def jidelnicek_plan():
 			nazev4 = session ['nazev4']
 		elif 'nazev5' in session:
 			nazev5 = session ['nazev5'] 
-#			return redirect(url_for('jidelnicek_plan_seznam'))
-		
+#			if request.method == 'POST':		
 		if form1.validate_on_submit():
-#				session['checkbox1'] = form1.checkbox1.data
-#				if session.get('checkbox') == True:
-				
-#						return render_template('jidelnicek3.html')
-				return redirect(url_for('jidelnicek_plan_seznam'))
+#						session['checkbox1'] = form1.checkbox1.data('checkbox1')
+#						if session.get('checkbox1') == True:
+						
+#							return render_template('jidelnicek3.html')
+			return redirect(url_for('jidelnicek_plan_seznam'))
+		else:
+			return render_template('jidelnicek2.html', nazev1=nazev1, nazev2=nazev2, nazev3=nazev3, nazev4=nazev4, nazev5=nazev5, ingre11=ingre11, ingre21=ingre21, ingre31=ingre31, ingre41=ingre41, ingre51=ingre51,form1=form1) #form2=form2)
 
-		return render_template('jidelnicek2.html', nazev1=nazev1, nazev2=nazev2, nazev3=nazev3, nazev4=nazev4, nazev5=nazev5, ingre11=ingre11, ingre21=ingre21, ingre31=ingre31, ingre41=ingre41, ingre51=ingre51,form1=form1) #form2=form2)
-	
 
 	#	form1 = IngreForm1()
 	#	if form1.validate_on_submit():
@@ -112,34 +121,15 @@ def jidelnicek_plan():
 
 
 #vygenerovani nakupniho seznamu
-@app.route('/jidelnicek_plan_seznam')
+@app.route('/jidelnicek_plan_seznam', methods=['GET', 'POST'])
 def jidelnicek_plan_seznam():
 		# 5 ruznych random receptu
-		nazev1 = Recepty.query.order_by(func.random()).first()
-		nazev2 = Recepty.query.order_by(func.random()).first()
-		nazev3 = Recepty.query.order_by(func.random()).first()
-		nazev4 = Recepty.query.order_by(func.random()).first()
-		nazev5 = Recepty.query.order_by(func.random()).first()
 
-		#zahrnuti ingredienci do receptu
-		ingre1 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev1.id).all()
-		ingre2 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev2.id).all()
-		ingre3 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev3.id).all()
-		ingre4 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev4.id).all()
-		ingre5 = db.session.query(Ingredience.ingredience).filter_by(recepty_id = nazev5.id).all()
-
-		ingre11 = [item[0] for item in ingre1]
-		ingre21 = [item[0] for item in ingre2]
-		ingre31 = [item[0] for item in ingre3]
-		ingre41 = [item[0] for item in ingre4]
-		ingre51 = [item[0] for item in ingre5]
-
-
-#		nazev1 = session.get('nazev1')
-#		nazev2 = session.get('nazev2')
-#		nazev3 = session.get('nazev3')
-#		nazev4 = session.get('nazev4')
-#		nazev5 = session.get('nazev5')
+		nazev1 = session.get('nazev1')
+		nazev2 = session.get('nazev2')
+		nazev3 = session.get('nazev3')
+		nazev4 = session.get('nazev4')
+		nazev5 = session.get('nazev5')
 
 #		if request.form.get('check_ingre'):
 #			return render_template('jidelnicek3.html', ingre11=ingre11, ingre21=ingre21, ingre31=ingre31,
@@ -149,7 +139,7 @@ def jidelnicek_plan_seznam():
 #				nazev4=nazev4, nazev5=nazev5, ingre11=ingre11, ingre21=ingre21, ingre31=ingre31, ingre41=ingre41,
 #				ingre51=ingre51)
 
-		return render_template('jidelnicek3.html', nazev1=nazev1, nazev2=nazev2, nazev3=nazev3, nazev4=nazev4, nazev5=nazev5, ingre11=ingre11, ingre21=ingre21, ingre31=ingre31, ingre41=ingre41, ingre51=ingre51)
+		return render_template('jidelnicek3.html', nazev1=nazev1, nazev2=nazev2, nazev3=nazev3, nazev4=nazev4, nazev5=nazev5)
 
 
 @app.errorhandler(404)
